523d46e749bd43ddffc4e23ff140ae317d554712
Leonid777999 <lskrypnikov@n-ix.com>
2023-04-09 19:18:49 +0300

    First commit

---
+++ constants/headers.py
@@ -0,0 +1,17 @@
+class Headers:
+
+
+    GET_BOOKING_HEADER = {
+     'Accept': 'application/json',
+    }
+
+    CREATE_BOOKING_HEADER = {
+     'Content-Type': 'application/json',
+     'Accept': 'application/json'
+    }
+
+    UPDATE_DELETE_BOOKING_HEADER = {
+     'Accept': 'application/json',
+     'Content-Type': 'application/json',
+     'Authorization': 'Basic YWRtaW46cGFzc3dvcmQxMjM='
+    }
---
+++ constants/url.py
@@ -0,0 +1,7 @@
+class Url:
+
+    GET_LIST = "https://restful-booker.herokuapp.com/booking"
+    GET_BOOKING = "https://restful-booker.herokuapp.com/booking/{id}"
+    CREATE = "https://restful-booker.herokuapp.com/booking"
+    UPDATE = "https://restful-booker.herokuapp.com/booking/{id}"
+    DELETE = "https://restful-booker.herokuapp.com/booking/{id}"
